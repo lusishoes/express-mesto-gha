cabc325dfc769b7f05341e4bf80ab3a62c93943b
Yury Gavrilov <gavrilov.invest@mail.ru>
2023-09-04 21:44:48 +0800

    15 new

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,7 +7,7 @@ const allowedCors = [
 ];
 
 const cors = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { origin } = req.headers.host; // Сохраняем источник запроса в переменную origin
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
